5f29ea6230896ea7f33dc6634867e64986dccbda
AlphaSgh <upupsgh@163.com>
2023-03-04 11:53:08 +0800

    2023-3-4 admin

--- Ssgh-Blog/src/main/java/com/ssgh/blog/controller/AdminController.java
+++ Ssgh-Blog/src/main/java/com/ssgh/blog/controller/AdminController.java
@@ -1,11 +1,14 @@
 package com.ssgh.blog.controller;
 
+import cn.hutool.captcha.ShearCaptcha;
+import com.ssgh.blog.entity.AdminUser;
 import com.ssgh.blog.service.AdminUserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,5 +27,90 @@ public class AdminController {
         request.setAttribute("path", "index");
 //        request.setAttribute("categoryCount",);
         return "admin/index";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("userName") String userName,
+                        @RequestParam("password") String password,
+                        @RequestParam("verifyCode") String verifyCode,
+                        HttpSession session) {
+        if (!StringUtils.hasText(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码不能为空");
+            return "admin/login";
+        }
+        if (!StringUtils.hasText(userName) || !StringUtils.hasText(password)) {
+            session.setAttribute("errorMsg", "用户名或密码不能为空");
+            return "admin/login";
+        }
+        ShearCaptcha shearCaptcha = (ShearCaptcha) session.getAttribute("verifyCode");
+        if (shearCaptcha == null || !shearCaptcha.verify(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码错误");
+            return "admin/login";
+        }
+
+        AdminUser adminUser = adminUserService.login(userName, password);
+        if (adminUser != null) {
+            session.setAttribute("loginUser", adminUser.getNickName());
+            session.setAttribute("loginUserId", adminUser.getAdminUserId());
+            session.setMaxInactiveInterval(60 * 60 * 2);
+            return "redirect:admin/index";
+        }
+        session.setAttribute("errorMsg", "登录失败");
+        return "admin/login";
+    }
+
+    @GetMapping("/profile")
+    public String profile(HttpServletRequest request) {
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        AdminUser adminUser = adminUserService.getUserDetailById(loginUserId);
+        if (adminUser == null) {
+            return "admin/login";
+        }
+        request.setAttribute("path", "profile");
+        request.setAttribute("loginUserName", adminUser.getNickName());
+        request.setAttribute("nickName", adminUser.getNickName());
+        return "admin/profile";
+    }
+
+    @PostMapping("/profile/password")
+    @ResponseBody
+    public String passwordUpdate(HttpServletRequest request, @RequestParam("originalPassword") String originalPassword,
+                                 @RequestParam("newPassword") String newPassword) {
+        if (!StringUtils.hasText(originalPassword) || !StringUtils.hasText(newPassword)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
+            request.getSession().removeAttribute("loginUserId");
+            request.getSession().removeAttribute("loginUser");
+            request.getSession().removeAttribute("errorMsg");
+            return "success";
+        } else {
+            return "修改失败";
+        }
+    }
+
+    @PostMapping("/profile/name")
+    @ResponseBody
+    public String nameUpdate(HttpServletRequest request,
+                             @RequestParam("loginUserName") String loginUserName,
+                             @RequestParam("nickName") String nickName) {
+        if (!StringUtils.hasText(loginUserName) || !StringUtils.hasText(nickName)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updateName(loginUserId, loginUserName, nickName)) {
+            return "success";
+        } else {
+            return "修改失败";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("loginUserId");
+        request.getSession().removeAttribute("loginUser");
+        request.getSession().removeAttribute("errorMsg");
+        return "admin/login";
     }
 }
--- Ssgh-Blog/src/main/java/com/ssgh/blog/service/AdminUserService.java
+++ Ssgh-Blog/src/main/java/com/ssgh/blog/service/AdminUserService.java
@@ -3,8 +3,37 @@ package com.ssgh.blog.service;
 import com.ssgh.blog.entity.AdminUser;
 
 public interface AdminUserService {
+
+    /**
+     *
+     * @param userName
+     * @param password
+     * @return
+     */
     AdminUser login(String userName, String password);
+
+    /**
+     *
+     * @param loginUserId
+     * @return
+     */
     AdminUser getUserDetailById(Integer loginUserId);
+
+    /**
+     *
+     * @param loginUserId
+     * @param originalPassword
+     * @param newPassword
+     * @return
+     */
     Boolean updatePassword(Integer loginUserId, String originalPassword, String newPassword);
+
+    /**
+     *
+     * @param loginUserId
+     * @param loginUserName
+     * @param nickName
+     * @return
+     */
     Boolean updateName(Integer loginUserId, String loginUserName, String nickName);
 }
